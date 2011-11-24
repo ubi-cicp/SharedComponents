fe7fca47cabae5d81a85b4296c4eae97bb85490d
Atsushi OHNO <atsushi-o@is.naist.jp>
2011-11-25 07:57:32 +0900

    ある1点を基準とした時のもう一点の方向を表すDirection列挙型を作成

--- src/org/ubilab/cicp2011/ShogiPiece.java
+++ src/org/ubilab/cicp2011/ShogiPiece.java
@@ -9,15 +9,122 @@ import java.io.Serializable;
  */
 public class ShogiPiece implements Serializable {
     /**
-     * 将棋駒の種類を表す列挙型
-     * <br>
+     * 方向を表す列挙型
+     * 
      * <pre>
-     * | 0| 1| 2| 3| 4|
-     * | 5| 6| 7| 8| 9|
-     * |10|11|駒|11|12|
-     * |13|14|15|16|17|
-     * |18|19|20|21|22|
+     *           ← x
+     * |00|01|02|03|04|*ref point
+     * |05|06|07|08|09|y
+     * |10|11|駒|12|13|↓
+     * |14|15|16|17|18|
+     * |19|20|21|22|23|
      * </pre>
+     * @author atsushi-o
+     * @since 2011/11/23
+     */
+    public enum Direction {
+        /**
+         * 一つのマスを中心とした5x5のグリッドそれぞれに対応する位置フラグ
+         */
+        D00 (2,-2), D01 (1,-2), D02 (0,-2), D03 (-1,-2), D04 (-2,-2),
+        D05 (2,-1), D06 (1,-1), D07 (0,-1), D08 (-1,-1), D09 (-2, 1),
+        D10 (2, 0), D11 (1, 0),             D12 (-1, 0), D13 (-2, 0),
+        D14 (2, 1), D15 (1, 1), D16 (0, 1), D17 (-1, 1), D18 (-2, 1),
+        D19 (2, 2), D20 (1, 2), D21 (0, 2), D22 (-1, 2), D23 (-2, 2),
+        /**
+         * グリッドに収まり切らない前方位置
+         */
+        FORWARD (0, -3),
+        /**
+         * グリッドに収まり切らない後方位置
+         */
+        DOWNWARD (0, 3),
+        /**
+         * グリッドに収まり切らない左方位置
+         */
+        LEFTWARD (3, 0),
+        /**
+         * グリッドに収まり切らない右方位置
+         */
+        RIGHTWARD (-3, 0),
+        /**
+         * グリッドに収まり切らない斜め位置
+         */
+        DIAGONAL (3, 3),
+        /**
+         * その他，割り当て不可能位置
+         */
+        OTHER (0, 0);
+        
+        private final int x;
+        private final int y;
+        
+        private Direction(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        /**
+         * ある駒を中心とした時の座標の差（右上が基準点）から方向を求める
+         * @param x 横方向座標の差
+         * @param y 縦方向座標の差
+         * @return 対応するDirection
+         * @since 2011/11/24
+         */
+        public static Direction valueOf(int x, int y) {
+            int absx = Math.abs(x);
+            int absy = Math.abs(y);
+            
+            // グリッド外かどうかの判定
+            if (absx >= 3 || absy >= 3) {
+                if (absx >=3 && absy >= 3) {
+                    if (absx == absy)   return Direction.DIAGONAL;
+                    else                return Direction.OTHER;
+                }
+                // 横方向のグリッド外位置
+                else if (absx >= 3) {
+                    if (y == 0) return x>0?Direction.LEFTWARD:Direction.RIGHTWARD;
+                    else        return Direction.OTHER;
+                }
+                // 縦方向のグリッド外位置
+                else {
+                    if (x == 0) return y<0?Direction.FORWARD:Direction.DOWNWARD;
+                    else        return Direction.OTHER;
+                }
+            }
+            
+            for (Direction d : values()) {
+                if (d.x == x && d.y == y) return d;
+            }
+            
+            throw new IllegalArgumentException();
+        }
+        
+        /**
+         * ある駒を中心とした時の座標の差（右上が基準点）から方向を求める
+         * @param diff 座標の差を表すShogiPos
+         * @return 対応するDirection
+         * @since 2011/11/24
+         */
+        public static Direction valueOf(ShogiPos diff) {
+            return Direction.valueOf(diff.x(), diff.y());
+        }
+        
+        /**
+         * srcの位置を中心とした時のdstの方向を求める
+         * @param src 基準とする位置
+         * @param dst 方向を求めたい位置
+         * @return 対応するDirection
+         * @since 2011/11/24
+         */
+        public static Direction valueOf(ShogiPos src, ShogiPos dst) {
+            ShogiPos diff = src.diff(dst);
+            return Direction.valueOf(diff);
+        }
+    }
+    
+    /**
+     * 将棋駒の種類を表す列挙型
      * @author atsushi-o
      * @since 2011/11/22
      */
